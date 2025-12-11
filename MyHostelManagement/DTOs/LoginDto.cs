39aa9459b7068b2a6eee68984accb328a6bc0979
Govind Suman <govindsuman777@gmail.com>
2025-12-11 15:12:30 +0530

    adding tenant role for login api

--- MyHostelManagement/DTOs/LoginDto.cs
+++ MyHostelManagement/DTOs/LoginDto.cs
@@ -4,5 +4,6 @@
     {
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public string UserRoleDate { get; set; } = string.Empty;
     }
 }
--- MyHostelManagement/Repositories/Implementations/LoginRepository.cs
+++ MyHostelManagement/Repositories/Implementations/LoginRepository.cs
@@ -18,9 +18,31 @@ namespace MyHostelManagement.Repositories.Implementations
 
         public virtual async Task<LoginResponseDTO> LoginAsync(LoginDto dto)
         {
-            
+            if (dto.UserRoleDate == "Tenant")
+            {
+                var tenantResponse = await _dbContext.Tenants
+                                      .Where(x => x.Phone == dto.Email).FirstOrDefaultAsync();
+
+                if (tenantResponse != null)
+                {
+                    return new LoginResponseDTO
+                    {
+                        id = tenantResponse.Id,
+                        message = null
+                    };
+                }
+                else
+                {
+                    return new LoginResponseDTO
+                    {
+                        id = new Guid(),
+                        message = "User Not Found"
+                    };
+                }
+            }
+
             var response = await _dbContext.AppUsers
-                                .Where(x => x.Email == dto.Email && x.PasswordHash == dto.Password).FirstOrDefaultAsync();
+                                .Where((x => x.Email == dto.Email || x.Phone == dto.Email  && x.PasswordHash == dto.Password)).FirstOrDefaultAsync();
 
             if (response != null)
             {

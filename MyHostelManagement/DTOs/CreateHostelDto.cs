d82127f18c9310a8363114c01baf84f64d3435bb
suman <govindsuman777@gmail.com>
2026-02-14 01:11:30 +0530

    updates

--- MyHostelManagement/DTOs/CreateHostelDto.cs
+++ MyHostelManagement/DTOs/CreateHostelDto.cs
@@ -6,5 +6,7 @@
         public string? Address { get; set; }
         public string? OwnerName { get; set; }
         public string PhoneNumber { get; set; }
+        public string Password { get; set; } = string.Empty;
+        public bool IsPasswordUpdated { get; set; } = false;
     }
 }
--- MyHostelManagement/Services/Implementations/HostelService.cs
+++ MyHostelManagement/Services/Implementations/HostelService.cs
@@ -1,9 +1,12 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using MyHostelManagement.Api.DTOs;
 using MyHostelManagement.Api.Models;
 using MyHostelManagement.Api.Services.Interfaces;
 using MyHostelManagement.DTOs;
 using MyHostelManagement.Repositories.Interfaces;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace MyHostelManagement.Api.Services.Implementations;
 
@@ -20,12 +23,17 @@ public class HostelService : IHostelService
 
     public async Task<HostelResponseDto> CreateAsync(CreateHostelDto dto)
     {
+        using var hmac = new HMACSHA256();
         var hostel = new Hostel
         {
             Name = dto.Name ?? string.Empty,
             Address = dto.Address,
             OwnerName = dto.OwnerName ?? string.Empty,
             PhoneNumber = dto.PhoneNumber,
+            PasswordHash = Convert.ToBase64String(
+                hmac.ComputeHash(Encoding.UTF8.GetBytes(dto.Password))
+            ),
+            PasswordSalt = Convert.ToBase64String(hmac.Key),
         };
 
         await _hostelRepo.CreateAsync(hostel);
@@ -51,11 +59,18 @@ public class HostelService : IHostelService
     {
         var hostel = await _hostelRepo.GetByIdAsync(id);
         if (hostel == null) return false;
-
+        using var hmac = new HMACSHA256();
         hostel.Name = dto.Name ?? string.Empty;
         hostel.Address = dto.Address;
         hostel.OwnerName = dto.OwnerName ?? string.Empty;
         hostel.PhoneNumber = dto.PhoneNumber;
+        if (dto.IsPasswordUpdated)
+        {
+            hostel.PasswordHash = Convert.ToBase64String(
+                hmac.ComputeHash(Encoding.UTF8.GetBytes(dto.Password))
+            );
+            hostel.PasswordSalt = Convert.ToBase64String(hmac.Key);
+        }
 
         await _hostelRepo.UpdateAsync(hostel);
         return true;

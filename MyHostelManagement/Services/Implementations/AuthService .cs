131c9135bc8518c10b892838a022ec49a6a8465a
suman <govindsuman777@gmail.com>
2026-02-22 11:50:54 +0530

    login api update

--- MyHostelManagement/Services/Implementations/AuthService .cs
+++ MyHostelManagement/Services/Implementations/AuthService .cs
@@ -28,61 +28,86 @@ namespace MyHostelManagement.Services.Implementations
 
         public async Task<AuthResponseDto> LoginAsync(LoginDto dto)
         {
-            if (dto != null && dto.Role.ToLower() == "Owner".ToLower())
-            {
-                var owner = await _hostelRepo.GetAllAsync();
-                var matchedOwner = owner.FirstOrDefault(h => h.PhoneNumber == dto.PhoneNumber);
-                if (matchedOwner == null)
-                    throw new UnauthorizedAccessException("Invalid credentials");
+            var user = await _userRepo.GetByPhoneAsync(dto.PhoneNumber);
+            if (user == null)
+                throw new UnauthorizedAccessException("Invalid credentials");
 
-                using var hmac1 = new HMACSHA256(
-                    Convert.FromBase64String(matchedOwner.PasswordSalt)
-                    );
-
-                var computedHash1 = Convert.ToBase64String(
-                hmac1.ComputeHash(Encoding.UTF8.GetBytes(dto.Password))
-                );
-
-                if (computedHash1 != matchedOwner.PasswordHash)
-                    throw new UnauthorizedAccessException("Invalid credentials");
-
-                var token = _jwtService.GenerateToken(matchedOwner, "Owner");
-
-                return new AuthResponseDto
-                {
-                    AccessToken = token,
-                    Role = "Owner",
-                    HostelId = matchedOwner.Id
-                };
-            }
-            else
-            {
-                var user = await _userRepo.GetByPhoneAsync(dto.PhoneNumber);
-                if (user == null)
-                    throw new UnauthorizedAccessException("Invalid credentials");
-
-                using var hmac = new HMACSHA256(
+            using var hmac = new HMACSHA256(
                     Convert.FromBase64String(user.PasswordSalt)
                 );
 
-                var computedHash = Convert.ToBase64String(
-                    hmac.ComputeHash(Encoding.UTF8.GetBytes(dto.Password))
-                );
+            var computedHash = Convert.ToBase64String(
+                hmac.ComputeHash(Encoding.UTF8.GetBytes(dto.Password))
+            );
 
-                if (computedHash != user.PasswordHash)
-                    throw new UnauthorizedAccessException("Invalid credentials");
+            var role = await _roleRepo.GetByIdAsync(user.RoleId);
+            if (role == null)
+                throw new UnauthorizedAccessException("User role not found, Contact administrator");
+            var token = _jwtService.GenerateToken(user, role.RoleName);
 
-                var role = await _roleRepo.GetByIdAsync(user.RoleId);
-                var token = _jwtService.GenerateToken(user, role.RoleName);
+            return new AuthResponseDto
+            {
+                AccessToken = token,
+                Role = role.RoleName,
+                UserId = user.Id,
+                HostelId = user.HostelId
+            };
 
-                return new AuthResponseDto
-                {
-                    AccessToken = token,
-                    Role = role.RoleName,
-                    UserId = user.Id,
-                    HostelId = user.HostelId
-                };
-            }
+            //if (dto != null && dto.Role.ToLower() == "Owner".ToLower())
+            //{
+            //    var owner = await _hostelRepo.GetAllAsync();
+            //    var matchedOwner = owner.FirstOrDefault(h => h.PhoneNumber == dto.PhoneNumber);
+            //    if (matchedOwner == null)
+            //        throw new UnauthorizedAccessException("Invalid credentials");
+
+            //    using var hmac1 = new HMACSHA256(
+            //        Convert.FromBase64String(matchedOwner.PasswordSalt)
+            //        );
+
+            //    var computedHash1 = Convert.ToBase64String(
+            //    hmac1.ComputeHash(Encoding.UTF8.GetBytes(dto.Password))
+            //    );
+
+            //    if (computedHash1 != matchedOwner.PasswordHash)
+            //        throw new UnauthorizedAccessException("Invalid credentials");
+
+            //    var token = _jwtService.GenerateToken(matchedOwner, "Owner");
+
+            //    return new AuthResponseDto
+            //    {
+            //        AccessToken = token,
+            //        Role = "Owner",
+            //        HostelId = matchedOwner.Id
+            //    };
+            //}
+            //else
+            //{
+            //    var user = await _userRepo.GetByPhoneAsync(dto.PhoneNumber);
+            //    if (user == null)
+            //        throw new UnauthorizedAccessException("Invalid credentials");
+
+            //    using var hmac = new HMACSHA256(
+            //        Convert.FromBase64String(user.PasswordSalt)
+            //    );
+
+            //    var computedHash = Convert.ToBase64String(
+            //        hmac.ComputeHash(Encoding.UTF8.GetBytes(dto.Password))
+            //    );
+
+            //    if (computedHash != user.PasswordHash)
+            //        throw new UnauthorizedAccessException("Invalid credentials");
+
+            //    var role = await _roleRepo.GetByIdAsync(user.RoleId);
+            //    var token = _jwtService.GenerateToken(user, role.RoleName);
+
+            //    return new AuthResponseDto
+            //    {
+            //        AccessToken = token,
+            //        Role = role.RoleName,
+            //        UserId = user.Id,
+            //        HostelId = user.HostelId
+            //    };
+            //}
         }
     }
 }
